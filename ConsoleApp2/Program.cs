f33ba4af8216ba533358308d24fff61a5dcd2b46
kaleem <asjadkaleem@gmail.com>
2019-01-25 20:15:00 -0700

    first commmit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name?");
+            string name = Console.ReadLine();
+           Console.WriteLine("Hello " + name);
+
+            Console.ReadLine();
+        }
+    }
+
+}
